a8eaab77bc74f842a363d3c2a17abb116030c254
Kelly Hougland <knhougland@gmail.com>
2018-04-23 20:02:42 -0500

    Added login inputs and basic button functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  @ViewChild('username') uname;
+  @ViewChild('password') pword;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+  }
+
+  signIn() {
+
+    if(this.uname.value=="admin" && this.pword.value =="admin"){
+      let alert = this.alertCtrl.create({
+        title: 'Login Successful',
+        subTitle: 'You are Logged In.',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
 
   }
 
