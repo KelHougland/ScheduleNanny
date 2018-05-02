c2919f521f5ce1b062cfe82cda97c58237bf3918
Kelly Hougland <knhougland@gmail.com>
2018-05-01 19:41:56 -0500

    added messages for login

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 /**
@@ -19,20 +19,31 @@ export class RegisterPage {
   @ViewChild('username') uname;
   @ViewChild('password') pword;
 
-  constructor(private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
   }
 
+  alert(message: string){
+    this.alertCtrl.create({
+      title: 'Hey, Listen!',
+      subTitle: message,
+      buttons: ['OK']
+    }).present();
+  }
+
+
   registerUser() {
     this.fire.auth.createUserWithEmailAndPassword(this.uname.value,this.pword.value)
     .then(data =>{
       console.log('got data',data)
+      this.alert("Welcome to our community!");
     })
     .catch(error => {
       console.log('got an error',error)
+      this.alert("Make sure your name is a valid email and your password is at least 6 characters long.")
     });
 
   }
